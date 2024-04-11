32fcb4a9f762994b82b4e1e1fb73f041608e5b06
jashdoadeless <jashdoadeless@users.noreply.github.com>
2024-04-11 11:27:31 +0100

    Update file
    This script will now also rename headers which are compatible with a direct import with Outlook Online.

--- ConvertFrom-Vcf.ps1
+++ ConvertFrom-Vcf.ps1
@@ -1,68 +1,108 @@
-param($fileName = "c:\users\dmarkle\downloads\All Contacts.vcf")
+
 
 function New-Card {
-    return [PSCustomObject][ordered]@{'Full Name'="";'Categories'='';'Organization'='';'Address'='';'Phone1'='';'Phone2'='';'Phone3'='';'Phone4'='';'Email1'='';'Email2'='';'Email3'='';'Email4'='';'Note'=''}
+    return [PSCustomObject][ordered]@{'First Name'="";'Categories'='';'Organization'='';'Address'='';'Mobile Phone'='';'Business Phone'='';'Business Phone 2'='';'Home Phone'='';'E-mail Address'='';'E-mail 2 Address'='';'E-mail 3 Address'='';'E-mail 4 Address'='';'Note'=''}
+}
+
+function Generate-CSVFromContactFile($filename){
+    $content = Get-Content $fileName
+    $cardCount = 0;
+    $currentCard = New-Card
+    #$state = 'out'
+    foreach ($line in $content) {
+        if ($line -match "^PRODID.*") {
+    #        $state = 'in'
+            $cardCount++;
+            $telephones = 0;
+            $emails = 0;
+            $telephonecolumnname = ""
+            $emailcolumnname = ""
+            $currentCard = New-Card
+            continue;
+        }
+        if ($line -match "^END:VCARD.*") {
+    #        $state = 'out'
+            $currentCard
+            continue;
+        }
+
+        if ($line -match "^PHOTO;.*") {
+    #        $state = 'photo'
+            continue;
+        }
+
+        if ($line -match "FN:.*") {
+            $tokens = $line -split ":"
+            $currentCard."First Name" = $tokens[1] -replace "\\,", ","
+        }
+
+        if ($line -match "ORG:.*") {
+            $tokens = $line -split ":"
+            $currentCard."Organization" = (($tokens[1] -split ';') -join "`n") -replace "\\,", ","
+        }
+
+        if ($line -match "^ADR.*:.*" -or $line -match "^item1.ADR.*:.*") {
+            $tokens = $line -split ":"
+            $currentCard."Address" = (($tokens[1] -split ';').Where({-not [string]::IsNullOrWhitespace($_)}) -join "`n") -replace "\\,", "," -replace "\\n", "`n"
+        }
+
+        if ($line -match "TEL;") {
+            $telephones++;
+            $tokens = $line -split ":"
+            switch ($telephones) {
+                1 {$telephonecolumnname = "Mobile Phone"}
+                2 {$telephonecolumnname = "Business Phone"}
+                3 {$telephonecolumnname = "Business Phone 2"}
+                4 {$telephonecolumnname = "Home Phone"}
+                default{}
+                }
+                $currentCard.$telephonecolumnname = (($tokens[1] -split ';') -join "`n") -replace "\\,", ","
+            
+        }
+        if ($line -match "Email;") {
+            $emails++;
+            $tokens = $line -split ":"
+             switch ($emails) {
+                1 {$emailcolumnname = "E-mail Address"}
+                2 {$emailcolumnname = "E-mail 2 Address"}
+                3 {$emailcolumnname = "E-mail 3 Address"}
+                4 {$emailcolumnname = "E-mail 4 Address"}
+                default{}
+                }
+            $currentCard.$emailcolumnname = (($tokens[1] -split ';') -join "`n") -replace "\\,", ","
+        }
+
+        if ($line -match "NOTE:") {
+            $telephones++;
+            $tokens = $line -split ":"
+            $currentCard."Note" = (($tokens[1] -split ';') -join "`n") -replace "\\,", ","
+        }
+
+        if ($line -match "CATEGORIES.*") {
+            $tokens = $line -split ":"
+            $currentCard."Categories" = $tokens[1]
+        }  
+    }
 }
 
 
-$content = Get-Content $fileName
-$cardCount = 0;
-$currentCard = New-Card
-$state = 'out'
-foreach ($line in $content) {
-    if ($line -match "^PRODID.*") {
-        $state = 'in'
-        $cardCount++;
-        $telephones = 0;
-        $emails = 0;
-        $currentCard = New-Card
-        continue;
-    }
-    if ($line -match "^END:VCARD.*") {
-        $state = 'out'
-        $currentCard
-        continue;
-    }
+do{
 
-    if ($line -match "^PHOTO;.*") {
-        $state = 'photo'
-        continue;
-    }
+$ErrorActionPreference = "Stop"
 
-    if ($line -match "FN:.*") {
-        $tokens = $line -split ":"
-        $currentCard."Full Name" = $tokens[1] -replace "\\,", ","
-    }
+$files = Get-ChildItem "C:\contact-conversion\inbound\*" -include *.vcf
 
-    if ($line -match "ORG:.*") {
-        $tokens = $line -split ":"
-        $currentCard."Organization" = (($tokens[1] -split ';') -join "`n") -replace "\\,", ","
-    }
 
-    if ($line -match "^ADR.*:.*" -or $line -match "^item1.ADR.*:.*") {
-        $tokens = $line -split ":"
-        $currentCard."Address" = (($tokens[1] -split ';').Where({-not [string]::IsNullOrWhitespace($_)}) -join "`n") -replace "\\,", "," -replace "\\n", "`n"
-    }
-
-    if ($line -match "TEL;") {
-        $telephones++;
-        $tokens = $line -split ":"
-        $currentCard."Phone$telephones" = (($tokens[1] -split ';') -join "`n") -replace "\\,", ","
-    }
-    if ($line -match "Email;") {
-        $emails++;
-        $tokens = $line -split ":"
-        $currentCard."Email$emails" = (($tokens[1] -split ';') -join "`n") -replace "\\,", ","
-    }
-
-    if ($line -match "NOTE:") {
-        $telephones++;
-        $tokens = $line -split ":"
-        $currentCard."Note" = (($tokens[1] -split ';') -join "`n") -replace "\\,", ","
-    }
-
-    if ($line -match "CATEGORIES.*") {
-        $tokens = $line -split ":"
-        $currentCard."Categories" = $tokens[1]
-    }  
+foreach ($file in $files) {
+     
+     $outboundfile =  "C:\contact-conversion\outbound\" + $file.BaseName + ".csv"
+     $renamedfile = $file.BaseName + ".vcf.bak"
+     Generate-CSVFromContactFile($file.FullName) | Export-Csv $outboundfile -NoTypeInformation
+    
+     Rename-Item -Path $file.FullName -NewName $renamedfile
+     write-host "completed $file.FullName"
 }
+
+Start-Sleep -Seconds 5
+}
+while ($true)
